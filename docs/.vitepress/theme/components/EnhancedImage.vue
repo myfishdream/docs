5b01e6b2a22a3bc86474413927117573b215bab6
鱼梦江湖 <zyyypx@qq.com>
2025-02-21 22:53:47 +0800

    灯箱图片

--- docs/.vitepress/theme/components/EnhancedImage.vue
+++ docs/.vitepress/theme/components/EnhancedImage.vue
@@ -10,17 +10,18 @@
         @load="handleLoad"
         @error="handleError"
         class="image"
+        :style="{ width: props.width, height: props.height }"
         ref="imageRef"
       />
       <div v-if="loading" class="loading-container">
         <Loading />
       </div>
-      <div v-if="error" class="error-container" @click.stop="retryLoad">
-        <div class="error-content">
-          <span class="error-icon">⚠️</span>
-          <span class="error-text">加载失败</span>  
-          <button class="retry-button">重试</button>
-        </div>
+      <div v-if="error" class="error-container" @click.stop="retryLoad" title="重试加载图片">
+        <span class="error-icon">
+          <svg t="1740145296421" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="26828" width="32" height="32">
+            <path d="M320 447.936c35.392 0 64-28.608 64-64s-28.608-64-64-64-64 28.608-64 64S284.608 447.936 320 447.936zM751.744 706.752c-146.496-91.008-333.44-91.008-479.872 0-14.336 8.896-19.328 28.736-11.264 44.48 8 15.744 26.048 21.248 40.384 12.352 128.704-79.936 292.864-79.936 421.632 0 4.544 2.88 9.6 4.224 14.528 4.224 10.368 0 20.416-6.016 25.856-16.64C771.072 735.488 766.016 715.648 751.744 706.752zM832 127.936 192 127.936c-70.656 0-128 57.344-128 128l0 576c0 70.656 57.344 128 128 128l640 0c70.656 0 128-57.344 128-128l0-576C960 185.28 902.656 127.936 832 127.936zM896 831.936c0 35.392-28.608 64-64 64L192 895.936c-35.392 0-64-28.608-64-64l0-576c0-35.392 28.608-64 64-64l640 0c35.392 0 64 28.608 64 64L896 831.936zM704 319.936c-35.392 0-64 28.608-64 64s28.608 64 64 64 64-28.608 64-64S739.392 319.936 704 319.936z" p-id="26829"></path>
+          </svg>
+        </span>
       </div>
     </div>
 
@@ -97,13 +98,13 @@ const props = defineProps({
 })
 
 // 状态管理
-const loading = ref(true)
-const error = ref(false)
-const retryCount = ref(0)
-const maxRetries = 3
-const visible = ref(false)
-const imageRef = ref(null)
-const lightbox = ref(null)
+const loading = ref(true) // 是否正在加载
+const error = ref(false) // 是否加载失败
+const retryCount = ref(0) // 重试次数
+const maxRetries = 3 // 最大重试次数
+const visible = ref(false) // 是否显示灯箱
+const imageRef = ref(null) // 图片元素引用
+const lightbox = ref(null) // 灯箱组件引用
 
 // 事件处理
 const handleLoad = () => {
--- docs/index/recording.md
+++ docs/index/recording.md
@@ -10,6 +10,6 @@ import Recording from '../.vitepress/theme/components/home/recording.vue'
 
 <Recording />
 
-
-
-  <EnhancedImage src="https://s21.ax1x.com/2025/01/31/pEZ9eG8.jpg" alt="本地图片" />
+<EnhancedImage src="https://s21.ax1x.com/2025/02/09/pEnPH0g.webp"  alt="background"  width="500px"  height="670px" />
+<EnhancedImage src="https://s21.ax1x.com/2025/01/31/pEZ9eG8.jpg"  alt="background"  width="500px"  />
+<EnhancedImage src="https://www.mocky.io/v2/5e8c0c1f3000006c00c1f3e0?mocky-delay=5000ms"  alt="background"  width="100px"  />
