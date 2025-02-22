293dc0fb140e6796148054786c7ebe7d91952480
鱼梦江湖 <zyyypx@qq.com>
2025-02-22 17:54:02 +0800

    优化

--- docs/.vitepress/theme/components/Loading.vue
+++ docs/.vitepress/theme/components/Loading.vue
@@ -1,57 +1,78 @@
 <style scoped>
-  .spinner {
-   width: 60px;
-   height: 60px;
-   position: relative;
+  .loader {
+    --color-success-300: #4bdf80;
+    --color-success-500: #1ed760;
+    --color-success-700: #18ac4d;
+    display: block;
+    justify-content: center;
+    align-items: center;
   }
 
-  .spinner .dot {
-   position: absolute;
-   inset: 0;
-   display: flex;
-   justify-content: center;
+  .loader .form1,
+  .loader .form2 {
+    display: flex;
+    justify-content: center;
+    align-items: center;
   }
 
-  .spinner .dot::after {
-   content: "";
-   width: 7px;
-   height: 7px;
-   border-radius: 50%;
-   background-color: rgb(12, 180, 231);
+  .square {
+    width: 40px;
+    height: 40px;
+    margin: 2px;
+    background-color: rgba(150, 150, 150, 0.253);
+    border-radius: 5px;
+    animation: blinking 0.8s ease-in-out infinite;
+    opacity: 0;
   }
 
-  @keyframes spin {
-   to {
-    transform: rotate(360deg);
-   }
+  @keyframes blinking {
+    0% {
+      opacity: 0.25;
+      background-color: rgba(4, 160, 30, 0.589);
+    }
+
+    50% {
+      opacity: 0.5;
+      background-color: var(--color-success-300);
+    }
+
+    75% {
+      opacity: 0.75;
+      background-color: var(--color-success-500);
+    }
+
+    100% {
+      opacity: 1;
+      background-color: var(--color-success-700);
+    }
   }
 
-  .spinner .dot {
-   animation: spin 2s infinite;
+  .form1 .square:nth-child(1) {
+    animation-delay: 0.2s;
   }
 
-  .spinner .dot:nth-child(2) {
-   animation-delay: 100ms;
+  .form1 .square:nth-child(2) {
+    animation-delay: 0.4s;
   }
 
-  .spinner .dot:nth-child(3) {
-   animation-delay: 200ms;
+  .form2 .square:nth-child(2) {
+    animation-delay: 0.6s;
   }
 
-  .spinner .dot:nth-child(4) {
-   animation-delay: 300ms;
-  }
-
-  .spinner .dot:nth-child(5) {
-   animation-delay: 400ms;
+  .form2 .square:nth-child(1) {
+    animation-delay: 0.8s;
   }
 </style>
 
 <template>
-  <div class="spinner">
-  <div class="dot"></div>
-  <div class="dot"></div>
-  <div class="dot"></div>
-  <div class="dot"></div>
-  <div class="dot"></div></div>
+  <div class="loader">
+    <div class="form1">
+      <div class="square"></div>
+      <div class="square"></div>
+    </div>
+    <div class="form2">
+      <div class="square"></div>
+      <div class="square"></div>
+    </div>
+  </div>
 </template>
--- docs/index/recording.md
+++ docs/index/recording.md
@@ -10,20 +10,9 @@ import Recording from '../.vitepress/theme/components/home/recording.vue'
 
 <Recording />
 
-## test
+## 醒狮助威
 
-<EnhancedImage src="https://s21.ax1x.com/2025/02/09/pEnPH0g.webp"  alt="background" />
+<EnhancedImage src="https://s21.ax1x.com/2025/02/22/pEl3qMt.jpg"  alt="醒狮助威" width='800px'/>
 
-
-
-## test
-
-<EnhancedImage src="https://s21.ax1x.com/2025/01/31/pEZ9eG8.jpg"  alt="background"  width="500px"  />
-
-
-
-## test
-
-<EnhancedImage src="https://www.mocky.io/v2/5e8c0c1f3000006c00c1f3e0?mocky-delay=5000ms"  alt="background"  width="100px"  />
 
 
