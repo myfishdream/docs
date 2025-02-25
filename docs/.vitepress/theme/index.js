db4471246ba9bb9438b55bd8d9a91b0ab02a9918
鱼梦江湖 <zyyypx@qq.com>
2025-02-25 20:52:55 +0800

    Picture

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -10,7 +10,7 @@ import MouseEvent from './components/MouseEvent.vue' // 鼠标跟随特效
 // import ThemeSwitch from './components/ThemeSwitch.vue' // 主题切换过渡动画
 import Classification from './components/classification.vue'  // 分类
 import ReadingProgress from './components/ReadingProgress.vue'  // 阅读进度
-import EnhancedImage from './components/EnhancedImage.vue' // 增强图片组件
+import Picture from './components/Picture.vue' // 增强图片组件
 import Timelines from './components/Timelines.vue' // 时间轴组件
 import DocsMsg from './components/docsMsg.vue' // 文档信息组件
 
@@ -45,7 +45,7 @@ export default {
     app.component('CopyBlock', CopyBlock);
     app.component('MouseEvent', MouseEvent);
     app.component('ReadingProgress', ReadingProgress);
-    app.component('EnhancedImage', EnhancedImage); // 注册增强图片组件
+    app.component('Picture', Picture); // 注册增强图片组件
     app.component('Timelines', Timelines); // 注册时间轴组件
     app.component('DocsMsg', DocsMsg); // 注册文档信息组件
     app.use(VueLazyloadNext, {
--- docs/index/recording.md
+++ docs/index/recording.md
@@ -12,8 +12,8 @@ import Recording from '../.vitepress/theme/components/home/recording.vue'
 
 ## 醒狮助威
 
-<EnhancedImage src="https://s21.ax1x.com/2025/02/22/pEl3qMt.jpg"  alt="醒狮助威" width='800px'/>
+<Picture src="https://s21.ax1x.com/2025/02/22/pEl3qMt.jpg"  alt="醒狮助威" width='800px'/>
 
-<EnhancedImage src="https://www.mocky.io/v2/5e8c0c1f3000006c00c1f3e0?mocky-delay=5000ms"  alt="加载错误演示图片" width='800px'/>
+<Picture src="https://www.mocky.io/v2/5e8c0c1f3000006c00c1f3e0?mocky-delay=5000ms"  alt="加载错误演示图片" width='800px'/>
 
 
