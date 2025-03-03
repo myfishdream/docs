799b60177dba31e45b30c84c23bd665265ce1b54
鱼梦江湖 <zyyypx@qq.com>
2025-03-03 20:52:00 +0800

    优化

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -20,6 +20,13 @@ export default defineConfig({
       // 添加自定义的警告框支持
       setupAlertBoxes(md);
     },
+    container: {
+      tipLabel: '提示',
+      warningLabel: '警告',
+      dangerLabel: '危险',
+      infoLabel: '信息',
+      detailsLabel: '详细信息'
+    }
   },
   lastUpdated: true,
   head: [
