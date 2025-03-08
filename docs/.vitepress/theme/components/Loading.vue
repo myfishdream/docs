8d6bca6a3c0e7f296a928e1e31ce7eafe925ce70
鱼梦江湖 <zyyypx@qq.com>
2025-03-08 09:39:24 +0800

    更新

--- docs/.vitepress/theme/components/Loading.vue
+++ docs/.vitepress/theme/components/Loading.vue
@@ -1,116 +1,48 @@
 <style scoped>
   .loader {
+    --color: #a5a5b0;
+    --size: 40px;
+    position: relative;
+    width: var(--size);
+    height: var(--size);
+  }
+
+  .loader span {
+    width: 100%;
+    height: 100%;
     position: absolute;
-    top: 50%;
-    left: 50%;
-    z-index: 10;
-    width: 160px;
-    height: 100px;
-    margin-left: -80px;
-    margin-top: -50px;
-    border-radius: 5px;
-    background: #1e3f57;
-    animation: dot1_ 3s cubic-bezier(0.55,0.3,0.24,0.99) infinite;
+    top: 0;
+    left: 0;
+    border: 1px solid var(--color);
+    border-bottom: 1px solid transparent;
+    border-radius: 100%;
+    animation: keyframes-rotate .8s infinite linear;
   }
 
-  .loader:nth-child(2) {
-    z-index: 11;
-    width: 150px;
-    height: 90px;
-    margin-top: -45px;
-    margin-left: -75px;
-    border-radius: 3px;
-    background: #3c517d;
-    animation-name: dot2_;
+  .loader svg {
+    width: 100%;
+    height: 100%;
+    padding: 25%;
+    fill: var(--color);
+    animation: keyframes-blink .8s infinite ease-in-out;
   }
 
-  .loader:nth-child(3) {
-    z-index: 12;
-    width: 40px;
-    height: 20px;
-    margin-top: 50px;
-    margin-left: -20px;
-    border-radius: 0 0 5px 5px;
-    background: #6bb2cd;
-    animation-name: dot3_;
-  }
-
-  @keyframes dot1_ {
-    3%,97% {
-      width: 160px;
-      height: 100px;
-      margin-top: -50px;
-      margin-left: -80px;
-    }
-
-    30%,36% {
-      width: 80px;
-      height: 120px;
-      margin-top: -60px;
-      margin-left: -40px;
-    }
-
-    63%,69% {
-      width: 40px;
-      height: 80px;
-      margin-top: -40px;
-      margin-left: -20px;
+  @keyframes keyframes-rotate {
+    0% {
+      transform: rotate(360deg);
     }
   }
 
-  @keyframes dot2_ {
-    3%,97% {
-      height: 90px;
-      width: 150px;
-      margin-left: -75px;
-      margin-top: -45px;
-    }
-
-    30%,36% {
-      width: 70px;
-      height: 96px;
-      margin-left: -35px;
-      margin-top: -48px;
-    }
-
-    63%,69% {
-      width: 32px;
-      height: 60px;
-      margin-left: -16px;
-      margin-top: -30px;
-    }
-  }
-
-  @keyframes dot3_ {
-    3%,97% {
-      height: 20px;
-      width: 40px;
-      margin-left: -20px;
-      margin-top: 50px;
-    }
-
-    30%,36% {
-      width: 8px;
-      height: 8px;
-      margin-left: -5px;
-      margin-top: 49px;
-      border-radius: 8px;
-    }
-
-    63%,69% {
-      width: 16px;
-      height: 4px;
-      margin-left: -8px;
-      margin-top: -37px;
-      border-radius: 10px;
+  @keyframes keyframes-blink {
+    50% {
+      opacity: 0.8;
     }
   }
 </style>
 
 <template>
-  <div class="container">
-  	<div class="loader"></div>
-  	<div class="loader"></div>
-  	<div class="loader"></div>
+  <div class="loader">
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M64 32C28.7 32 0 60.7 0 96v64c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V96c0-35.3-28.7-64-64-64H64zm280 72a24 24 0 1 1 0 48 24 24 0 1 1 0-48zm48 24a24 24 0 1 1 48 0 24 24 0 1 1 -48 0zM64 288c-35.3 0-64 28.7-64 64v64c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V352c0-35.3-28.7-64-64-64H64zm280 72a24 24 0 1 1 0 48 24 24 0 1 1 0-48zm56 24a24 24 0 1 1 48 0 24 24 0 1 1 -48 0z"></path></svg>
+    <span></span>
   </div>
 </template>
